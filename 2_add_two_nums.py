fc4ee19e034d510bddef2e8111f59aaeec73ae53
Zhuoyuan <chenzhuoyuan07@gmail.com>
2017-03-23 00:45:28 -0700

    leet code 2

---
+++ 2_add_two_nums.py
@@ -0,0 +1,54 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if l1 is None:
+            return l2
+        if l2 is None:
+            return l1
+        tmp_l1 = l1
+        tmp_l2 = l2
+        l3_head = None
+        tmp_l3_last = l3_head
+        carry_flag = False
+        while tmp_l1 is not None or tmp_l2 is not None:
+            # create a new node
+            if tmp_l1 is not None and tmp_l2 is not None:
+                tmp_l3 = ListNode(tmp_l1.val + tmp_l2.val)
+            elif tmp_l1 is not None:
+                tmp_l3 = ListNode(tmp_l1.val)
+            else:
+                tmp_l3 = ListNode(tmp_l2.val)
+            if carry_flag:
+                tmp_l3.val += 1
+            if tmp_l3.val >= 10:
+                carry_flag = True
+                tmp_l3.val -= 10
+            else:
+                carry_flag = False
+                
+            # append    
+            if tmp_l3_last is None: # head   
+                l3_head = tmp_l3
+            else:
+                tmp_l3_last.next = tmp_l3
+            tmp_l3_last = tmp_l3
+            # move forward
+            if tmp_l1:
+                tmp_l1 = tmp_l1.next
+            if tmp_l2:
+                tmp_l2 = tmp_l2.next
+        # append the last 1 if necessary
+        if carry_flag:
+            tmp_l3 = ListNode(1)
+            tmp_l3_last.next = tmp_l3
+        return l3_head
