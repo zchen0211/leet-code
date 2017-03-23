dbdaff9ab03c7fedd4df3811ff0ff8d55646e105
Zhuoyuan <chenzhuoyuan07@gmail.com>
2017-03-23 01:26:06 -0700

    Create 541

---
+++ 541_reverse_str.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def reverseStr(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        s = list(s)
+        for i in xrange(0, len(s), 2*k):
+            s[i:i+k] = reversed(s[i:i+k])
+        result = ''.join(s)
+        return result
