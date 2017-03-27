cc3d3e9387708eb3482fe01ec470dc4cf0db7732
Zhuoyuan <chenzhuoyuan07@gmail.com>
2017-03-27 02:28:41 -0700

    6 more problems

---
+++ 160_intersect_of_llist.py
@@ -0,0 +1,46 @@
+'''
+Write a program to find the node at which the intersection of two singly linked lists begins.
+
+
+For example, the following two linked lists:
+
+A:          a1 → a2
+                   ↘
+                     c1 → c2 → c3
+                   ↗            
+B:     b1 → b2 → b3
+begin to intersect at node c1.
+
+
+Notes:
+
+If the two linked lists have no intersection at all, return null.
+The linked lists must retain their original structure after the function returns.
+You may assume there are no cycles anywhere in the entire linked structure.
+Your code should preferably run in O(n) time and use only O(1) memory.
+'''
+
+# Definition for singly-linked list.
+class ListNode(object):
+  def __init__(self, x):
+    self.val = x
+    self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        tmp1 = headA
+        tmp2 = headB
+        while(tmp1 != tmp2):
+            if tmp1 is not None:
+                tmp1 = tmp1.next
+            else:
+                tmp1 = headB
+            if tmp2 is not None:
+                tmp2 = tmp2.next
+            else:
+                tmp2 = headA
+        return tmp1
---
+++ 189_rotate_array.py
@@ -0,0 +1,15 @@
+'''
+Rotate an array of n elements to the right by k steps.
+
+For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+'''
+
+class Solution(object):
+  def rotate(self, nums, k):
+    k = k % len(nums) # a must!
+    nums[:] = nums[-k:] + nums[:-k]
+
+
+if __name__ == '__main__':
+  a = Solution()
+  print a.rotate(range(1,3), 1)
