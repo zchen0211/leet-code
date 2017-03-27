43d2ef49e594556c748504c6761950d9c639207c
Zhuoyuan <chenzhuoyuan07@gmail.com>
2017-03-27 03:36:16 -0700

    4 more questions

---
+++ 168_convert_sheet.py
@@ -0,0 +1,53 @@
+'''
+Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+
+For example:
+
+    1 -> A
+    2 -> B
+    3 -> C
+    ...
+    26 -> Z
+    27 -> AA
+    28 -> AB 
+'''
+
+# 1-26: A-Z
+# 26 + 1..26*26: AA..ZZ
+# cumul + 1..26*26*26:
+class Solution(object):
+  def convertToTitle(self, n):
+    """
+    :type n: int
+    :rtype: str
+    """
+    cumul = 0
+    tmp_dic = {}
+    for i in range(1, 27):
+      tmp_dic[i] = str(unichr(ord('A')+i-1))
+    i = 1
+    while(True):
+      cumul_after = cumul + 26 ** i
+      if n>cumul and n<=cumul_after:
+        result = ''
+        while(n>0):
+          rem = (n-1)%26 +1
+          n = (n-rem)/26
+          tmp_c = tmp_dic[rem]
+          result += tmp_c
+          # print rem, tmp_c
+        if len(result) < i:
+          result += 'A'*(i-len(result))
+        result = result[::-1]
+        return result
+      else:
+        cumul = cumul_after
+        i += 1
+
+
+if __name__ == '__main__':
+  a = Solution()
+  print a.convertToTitle(26)
+  print a.convertToTitle(28)
+  print a.convertToTitle(26+26*26+26**3)
+  
---
+++ 171_excel.py
@@ -0,0 +1,32 @@
+'''
+Related to question Excel Sheet Column Title
+
+Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+For example:
+
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28 
+'''
+
+class Solution(object):
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        tmp_dic ={}
+        for i in range(1,27):
+            tmp_dic[str(unichr(ord('A')+i-1))] = i
+        cnt = 0
+        s = s[::-1]
+        i = 0
+        for i in range(len(s)):
+            c = s[i]
+            cnt += tmp_dic[c] * (26**i)
+        return cnt
