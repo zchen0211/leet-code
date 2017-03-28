9ee19f31a939541e3c5da4e6da5d4a771e507bd5
Zhuoyuan <chenzhuoyuan07@gmail.com>
2017-03-28 03:03:07 -0700

    187 179 198

---
+++ hash_table/187_repeated_dna.py
@@ -0,0 +1,29 @@
+'''
+All DNA is composed of a series of nucleotides abbreviated as A, C, G, and T, for example: "ACGAATTCCG". When studying DNA, it is sometimes useful to identify repeated sequences within the DNA.
+
+Write a function to find all the 10-letter-long sequences (substrings) that occur more than once in a DNA molecule.
+
+For example,
+
+Given s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT",
+
+Return:
+["AAAAACCCCC", "CCCCCAAAAA"].
+'''
+
+class Solution(object):
+    def findRepeatedDnaSequences(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        n = len(s)
+        tmp_set = {}
+        for i in range(n-9):
+            if tmp_set.has_key(s[i:i+10]):
+                tmp_set[s[i:i+10]] += 1
+            else:
+                tmp_set[s[i:i+10]] = 1
+        # print tmp_set
+        result = [k for k, v in tmp_set.items() if v>1]
+        return result
---
+++ string/179_largest_num.py
@@ -0,0 +1,19 @@
+'''
+Given a list of non negative integers, arrange them such that they form the largest number.
+
+For example, given [3, 30, 34, 5, 9], the largest formed number is 9534330.
+
+Note: The result may be very large, so you need to return a string instead of an integer.
+'''
+
+class Solution(object):
+  def largestNumber(self, nums):
+    nums = [str(item) for item in nums]
+    # sort in reverse order
+    nums.sort(cmp=lambda x, y: cmp(y+x, x+y))
+    result = ''.join(nums)
+    result = result.lstrip('0')
+    if len(result) == 0:
+      return '0'
+    else
+      return result
