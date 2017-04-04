6df956f83a3aabcbbf05e49995311aed8f57ebc2
Zhuoyuan <chenzhuoyuan07@gmail.com>
2017-04-04 03:31:12 -0700

    240 241

--- 240_search_2d.py
+++ 240_search_2d.py
@@ -27,53 +27,16 @@ class Solution(object):
     n = len(matrix[0])
     if n == 0:
       return False
-    i, j = 0, 0
-    while(i<m and j<n):
-      if matrix[i][j] > target:
-        break
+    i, j = 0, n-1
+    while(j>=0 and i<=m-1):
       if matrix[i][j] == target:
         return True
-      else: # matrix too small
-        if i==m-1 and j==n-1:
-          return False
-        elif i<m-1 and j<n-1:
-          i, j = i+1, j+1
-        elif i==m-1 and j<n-1:
-          j += 1
-        elif j==n-1 and i<m-1:
-          i += 1
-    # search in matrix[0..i][j]
-    st = 0
-    end = i
-    while(st<end):
-      mid = (st+end)/2
-      if matrix[mid][j] == target:
-        return True
-      elif matrix[mid][j] < target:
-        st = mid+1
-      elif matrix[mid][j] > target:
-        end = mid-1
-    if st>0 and st<=i and matrix[st][j] == target:
-      return True
-    if end>0 and end<=i and matrix[end][j] == target:
-      return True
-    # search in matrix[i][0..j]
-    st = 0
-    end = j
-    while(st<end):
-      mid = (st+end)/2
-      if matrix[i][mid] == target:
-        return True
-      elif matrix[i][mid] < target:
-        st = mid+1
-      elif matrix[i][mid] > target:
-        end = mid-1
-    if st>0 and st<=j and matrix[i][st] == target:
-      return True
-    if end>0 and end<=j and matrix[i][end] == target:
-      return True
+      elif target < matrix[i][j]:
+        j -= 1
+      else:
+        i += 1
     return False
-
+    
 
 if __name__ == '__main__':
   a = Solution()
---
+++ 241_add_parentheses.py
@@ -0,0 +1,70 @@
+'''
+Given a string of numbers and operators, return all possible results from computing all the different possible ways to group numbers and operators. The valid operators are +, - and *.
+
+
+Example 1
+Input: "2-1-1".
+
+((2-1)-1) = 0
+(2-(1-1)) = 2
+Output: [0, 2]
+
+
+Example 2
+Input: "2*3-4*5"
+
+(2*(3-(4*5))) = -34
+((2*3)-(4*5)) = -14
+((2*(3-4))*5) = -10
+(2*((3-4)*5)) = -10
+(((2*3)-4)*5) = 10
+Output: [-34, -14, -10, -10, 10]
+'''
+
+class Solution(object):
+  def helper(self, op_list):
+    if len(op_list) == 1:
+      return [op_list[0]]
+    else:
+      result = []
+      for i in range(1, len(op_list)-1, 2):
+        res1 = self.helper(op_list[:i])
+        res2 = self.helper(op_list[i+1:])
+        if op_list[i] == '+':
+          res = [item1+item2 for item1 in res1 for item2 in res2]
+        if op_list[i] == '-':
+          res = [item1-item2 for item1 in res1 for item2 in res2]
+        if op_list[i] == '*':
+          res = [item1*item2 for item1 in res1 for item2 in res2]
+        result += res
+      return result
+
+  def diffWaysToCompute(self, inputs):
+    # first split
+    neg_flag = (inputs[0] == '-')
+    if neg_flag:
+      inputs = inputs[1:]
+    op_list = []
+    i = 0
+    while(i<len(inputs)):
+      if inputs[i] not in ['+', '-', '*']:
+        j = i
+        while(j<len(inputs)):
+          if j+1<len(inputs) and inputs[j+1] not in ['+','-','*']:
+            j += 1
+          else:
+            break
+        op_list.append(int(inputs[i:j+1]))
+        i = j
+      else:
+        op_list.append(inputs[i])
+      i += 1
+    if neg_flag:
+      op_list[0] = -op_list[0]
+    print op_list
+    return self.helper(op_list)
+
+
+if __name__ == '__main__':
+  a = Solution()
+  print a.diffWaysToCompute('2-1-1')
