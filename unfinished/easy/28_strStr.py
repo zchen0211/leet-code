182d7e8e8bc11b563b63bfeac1ceae95f575e65f
Zhuoyuan <chenzhuoyuan07@gmail.com>
2017-05-28 20:45:58 -0700

    27-30

---
+++ unfinished/easy/28_strStr.py
@@ -0,0 +1,16 @@
+'''
+28. Implement strStr() (Easy)
+
+Implement strStr().
+
+Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+'''
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        
