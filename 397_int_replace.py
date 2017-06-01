1c111921a20c0cbbf5b9b54982eaffb93c005ad8
Zhuoyuan <chenzhuoyuan07@gmail.com>
2017-06-01 02:06:46 -0700

    398

---
+++ 397_int_replace.py
@@ -0,0 +1,65 @@
+"""
+397. Integer Replacement (Medium)
+
+Given a positive integer n and you can do operations as follow:
+
+If n is even, replace n with n/2.
+If n is odd, you can replace n with either n + 1 or n - 1.
+What is the minimum number of replacements needed for n to become 1?
+
+Example 1:
+
+Input:
+8
+
+Output:
+3
+
+Explanation:
+8 -> 4 -> 2 -> 1
+Example 2:
+
+Input:
+7
+
+Output:
+4
+
+Explanation:
+7 -> 8 -> 4 -> 2 -> 1
+or
+7 -> 6 -> 3 -> 2 -> 1
+"""
+
+class Solution(object):
+  def integerReplacement(self, n):
+    """
+    :type n: int
+    :rtype: int
+    """
+    look_up_map = {1:0, 2:1, 3:2, 4:2}
+    r = self.helper(n, look_up_map)
+    print look_up_map
+    return r
+    
+  def helper(self, n, look_up_map):
+    offset = 0
+    ori_n = n
+    while(n%2==0):
+      n /= 2
+      offset += 1
+
+    if n in look_up_map:
+      look_up_map[ori_n] = look_up_map[n] + offset
+      return offset + look_up_map[n]
+    r1 = self.helper(n+1, look_up_map)
+    r2 = self.helper(n-1, look_up_map)
+    result = min(r1, r2) + 1 + offset
+    look_up_map[ori_n] = result
+    return result
+
+if __name__ == '__main__':
+  a = Solution()
+  # print a.integerReplacement(16)
+  # print a.integerReplacement(7)
+  print a.integerReplacement(1234)
---
+++ sampling/398_random_pick.py
@@ -0,0 +1,47 @@
+'''
+398. Random Pick Index (Medium)
+
+Given an array of integers with possible duplicates, randomly output the index of a given target number. You can assume that the given target number must exist in the array.
+
+Note:
+The array size can be very large. Solution that uses too much extra space will not pass the judge.
+
+Example:
+
+int[] nums = new int[] {1,2,3,3,3};
+Solution solution = new Solution(nums);
+
+// pick(3) should return either index 2, 3, or 4 randomly. Each index should have equal probability of returning.
+solution.pick(3);
+
+// pick(1) should return 0. Since in the array only nums[0] is equal to 1.
+solution.pick(1);
+'''
+
+import random
+
+class Solution(object):
+    def __init__(self, nums):
+        """
+        
+        :type nums: List[int]
+        :type numsSize: int
+        """
+        self.nums = nums
+        
+    def pick(self, target):
+        """
+        :type target: int
+        :rtype: int
+        """
+        n = len(self.nums)
+        result = -1
+        cnt = 0
+        for i in range(n):
+            if self.nums[i] != target:
+                continue
+            else:
+                cnt += 1
+                if random.randint(0,n) % cnt == 0:
+                    result = i
+        return result
