83fcb61246461da02e292e8d4dcf878565462069
Zhuoyuan <chenzhuoyuan07@gmail.com>
2017-06-05 00:36:38 -0700

    116-130

---
+++ unfinished/easy/118_pascal_tri.py
@@ -0,0 +1,24 @@
+'''
+118. Pascal's Triangle (Easy)
+
+Given numRows, generate the first numRows of Pascal's triangle.
+
+For example, given numRows = 5,
+Return
+
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+'''
+
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        
