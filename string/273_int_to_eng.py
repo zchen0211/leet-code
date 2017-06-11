c013fed70bfffa5dba71086678b5bd0de8250653
Zhuoyuan <chenzhuoyuan07@gmail.com>
2017-06-11 02:00:53 -0700

    more string

--- string/273_int_to_eng.py
+++ string/273_int_to_eng.py
@@ -41,36 +41,55 @@ class Solution(object):
       if result != '': result += ' '
       result += self.helper(num)
     return result
+
+  def solution2(self, num):
+    if num == 0: return 'Zero'
+    result = []
+    base = 10**9
+    if num > base:
+      result.append(self.helper(num/base) + ' Billion')
+      num = num % base
+    base = 10**6
+    if num > base:
+      result.append(self.helper(num/base) + ' Million')
+      num = num % base
+    base = 10**3
+    if num > base:
+      result.append(self.helper(num/base) + ' Thousand')
+      num = num % base
+    if num > 0:
+      result.append(self.helper(num))
+    # print result
+    result = ' '.join(result)
+    return result
   
   def helper(self, num):
-    result = ''
+    if num == 0: return 'Zero'
+    result = []
     # 0 - 999
-    map1 = {1:'One', 2:'Two', 3: 'Three', 4:'Four', 5:'Five', 6:'Six', 7:'Seven', 8:'Eight', 9:'Nine'}
-    map2 = {10: 'Ten', 11:'Eleven', 12:'Twelve', 13: 'Thirteen', 14:'Fourteen', 15:'Fifteen', 16:'Sixteen', 17:'Seventeen', 18:'Eighteen', 19:'Nineteen'}
+    map1 = {1:'One', 2:'Two', 3: 'Three', 4:'Four', 5:'Five', 6:'Six', 7:'Seven', 8:'Eight', 9:'Nine', 10: 'Ten', 11:'Eleven', 12:'Twelve', 13: 'Thirteen', 14:'Fourteen', 15:'Fifteen', 16:'Sixteen', 17:'Seventeen', 18:'Eighteen', 19:'Nineteen'}
     map3 = {20: 'Twenty', 30:'Thirty', 40: 'Forty', 50:'Fifty', 60: 'Sixty', 70: 'Seventy', 80: 'Eighty', 90:'Ninety'}
 
-    if num>=100:
-      tmp = num / 100
-      result += map1[tmp] + ' Hundred'
+    if num >= 100:
+      result.append(map1[num/100] + ' Hundred')
       num = num % 100
-    if num>=10 and num<=19:
-      if result != '': result += ' '
-      result += map2[num]
-      num = 0
-    if num>=20:
-      tmp = num / 10
-      if result != '': result += ' '
-      result += map3[tmp*10]
+    if num >= 20:
+      result.append(map3[(num/10)*10])
       num = num % 10
-    if num in map1:
-      if result != '': result += ' '
-      result += map1[num]
-    return result
+    if num<=19 and num>0:
+      result.append(map1[num])
+    return ' '.join(result)
 
 
 if __name__ == '__main__':
   a = Solution()
   print a.numberToWords(123)
+  print a.solution2(123)
   print a.numberToWords(12345)
+  print a.solution2(12345)
   print a.numberToWords(1234567)
+  print a.solution2(1234567)
   print a.numberToWords(1234567891)
+  print a.solution2(1234567891)
+  print a.solution2(0)
+  print a.solution2(1000)
