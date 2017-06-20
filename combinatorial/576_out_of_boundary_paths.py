1e87c6abf0c0f8d201cf6cd9c70308b7c5bea164
Zhuoyuan <chenzhuoyuan07@gmail.com>
2017-06-20 14:22:16 -0700

    576

--- combinatorial/576_out_of_boundary_paths.py
+++ combinatorial/576_out_of_boundary_paths.py
@@ -20,12 +20,49 @@ N is in range [0,50].
 '''
 
 class Solution(object):
-    def findPaths(self, m, n, N, i, j):
-        """
-        :type m: int
-        :type n: int
-        :type N: int
-        :type i: int
-        :type j: int
-        :rtype: int
-        """
+  def findPaths(self, m, n, N, i, j):
+    """
+    :type m: int
+    :type n: int
+    :type N: int
+    :type i: int
+    :type j: int
+    :rtype: int
+    """
+    if N == 0: return 0
+    # create an m x n matrix
+    matrix = []
+    for ii in range(m):
+      matrix.append([0]*n)
+    # initialize
+    for ii in range(n): matrix[0][ii] += 1
+    for ii in range(n): matrix[m-1][ii] += 1
+    for ii in range(m): matrix[ii][0] += 1
+    for ii in range(m): matrix[ii][n-1] += 1
+    # new_matrix for update
+    new_matrix = []
+    for ii in range(m):
+      new_matrix.append([0]*n)
+    cnt = matrix[i][j]
+    for step in range(N-1):
+      new_matrix, matrix = self.update(matrix, new_matrix, m, n)
+      cnt += matrix[i][j]
+      cnt = cnt  % (10**9+7)
+    return cnt
+
+
+  def update(self, matrix, new_matrix, m, n):
+    for i in range(m):
+      for j in range(n):
+        new_matrix[i][j] = 0
+        if i>0: new_matrix[i][j] += matrix[i-1][j] % (10**9+7)
+        if i<m-1: new_matrix[i][j] += matrix[i+1][j] % (10**9+7)
+        if j>0: new_matrix[i][j] += matrix[i][j-1] % (10**9+7)
+        if j<n-1: new_matrix[i][j] += matrix[i][j+1] % (10**9+7)
+    return matrix, new_matrix
+
+
+if __name__ == '__main__':
+  a = Solution()
+  print a.findPaths(1, 3, 3, 0, 1)
+  print a.findPaths(2, 2, 2, 0, 0)
