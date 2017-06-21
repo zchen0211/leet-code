541b0db69d8b5c9d348cba800511f95eccae7ce8
Zhuoyuan <chenzhuoyuan07@gmail.com>
2017-06-21 13:52:39 -0700

    all hard checked-in

---
+++ dp/464_can_I_win.py
@@ -0,0 +1,81 @@
+"""
+464. Can I Win (Medium)
+
+In the "100 game," two players take turns adding, to a running total, any integer from 1..10. The player who first causes the running total to reach or exceed 100 wins.
+
+What if we change the game so that players cannot re-use integers?
+
+For example, two players might take turns drawing from a common pool of numbers of 1..15 without replacement until they reach a total >= 100.
+
+Given an integer maxChoosableInteger and another integer desiredTotal, determine if the first player to move can force a win, assuming both players play optimally.
+
+You can always assume that maxChoosableInteger will not be larger than 20 and desiredTotal will not be larger than 300.
+
+Example
+
+Input:
+maxChoosableInteger = 10
+desiredTotal = 11
+
+Output:
+false
+
+Explanation:
+No matter which integer the first player choose, the first player will lose.
+The first player can choose an integer from 1 up to 10.
+If the first player choose 1, the second player can only choose integers from 2 up to 10.
+The second player will win by choosing 10 and get a total = 11, which is >= desiredTotal.
+Same with other integers chosen by the first player, the second player will always win.
+"""
+import timeit
+
+class Solution(object):
+  def canIWin(self, maxChoosableInteger, desiredTotal):
+    """
+    :type maxChoosableInteger: int
+    :type desiredTotal: int
+    :rtype: bool
+    """
+    n = maxChoosableInteger
+    if n*(n+1) < desiredTotal: return False
+    curr = [True] * n
+    # key = self.bool_to_int(curr)
+    table = {}
+    result = self.helper(table, curr, desiredTotal, n)
+    print table
+    return result
+
+  def helper(self, table, curr, total, n):
+    # case 1: already checked
+    key = tuple(curr)
+    if tuple(curr) in table:
+      return table[tuple(curr)]
+    # base case 2: achievable by select the largest number
+    # curr = self.int_to_bool(key, n)
+    # print 'current', curr
+
+    # memoized dp
+    for i in range(n-1,-1,-1):
+      if curr[i] == True:
+        # print 'total', total, 'i+1', i+1, curr
+        curr[i] = False
+        if total-i-1>0:
+          result = self.helper(table, curr, total-i-1, n)
+          # print table
+        else:
+          result = False
+        curr[i] = True
+        if not result:
+          table[key] = True
+          return True
+    table[key] = False
+    # print table
+    return False
+
+
+if __name__ == '__main__':
+  start = timeit.timeit()
+  a = Solution()
+  print a.canIWin(5,6) 
+  end = timeit.timeit()
+  print end-start
--- unfinished/hard/564_find_closest_palin.py
+++ unfinished/hard/564_find_closest_palin.py
@@ -14,8 +14,59 @@ If there is a tie, return the smaller one as answer.
 '''
 
 class Solution(object):
-    def nearestPalindromic(self, n):
-        """
-        :type n: str
-        :rtype: str
-        """
+  def nearestPalindromic(self, n):
+    """
+    :type n: str
+    :rtype: str
+    """
+    if n == '0': return '1'
+    if len(n) == 1:
+      return str(int(n)-1)
+
+    n_list = []
+    for i in range(len(n)): n_list.append(n[i])
+    n_list = [int(item) for item in n_list]
+
+  def isPalin(self, s):
+    i = 0
+    j = len(s)-1
+    while i<j:
+      if s[i] == s[i]:
+        i, j = i+1, j-1
+      else: return False
+    return True
+
+  def helper_smaller(self, s):
+    n = len(s)
+    if self.isPalin(s):
+      # decrese the first non-zero near center symmetrically
+      if n % 2 == 1:
+        i, j = (n-1)/2
+      else:
+        i, j = n/2-1, n/2
+      while i>=0:
+        if s[i] > 0:
+          s[i], s[j] = s[i]-1, s[j]-1 
+          break
+        i, j = i-1, j+1
+      # if not 100001
+      if sum(s) != 0: return s
+      else: return [9]*(n-1)
+    else:
+      # first difference take smaller
+      i = 0
+      j = n-1
+      # center '99999'
+      while 
+
+  def helper_larger(self, s):
+    if self.isPalin(s):
+      # increase the first non-9 digit symmetrically
+      # if all 9, 100001
+    else: # not palin
+      # first difference take larger
+      # all center '00000'
+
+if __name__ == '__main__':
+  a = Solution()
+  print a.nearestPalindromic('12345')
