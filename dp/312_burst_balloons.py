0354e9b1a16fff0e00ad86a4b638835fd1459bda
Zhuoyuan <chenzhuoyuan07@gmail.com>
2017-08-08 16:25:03 -0700

    312 burst balloons

--- dp/312_burst_balloons.py
+++ dp/312_burst_balloons.py
@@ -7,7 +7,7 @@ Find the maximum coins you can collect by bursting the balloons wisely.
 
 Note: 
 (1) You may imagine nums[-1] = nums[n] = 1. They are not real therefore you can not burst them.
-(2) 0 ≤ n ≤ 500, 0 ≤ nums[i] ≤ 100
+(2) 0 <= n <= 500, 0 <= nums[i] <= 100
 
 Example:
 
@@ -20,9 +20,34 @@ Return 167
 '''
 
 class Solution(object):
-    def maxCoins(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        
+  def maxCoins(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    nums.insert(0, 1)
+    nums.append(1)
+    self.table = {}
+    n = len(nums)
+    res = self.helper(nums, 0, n-1)
+    print self.table
+    return res
+
+  def helper(self, nums, i, j):
+    if (i,j) in self.table:
+      return self.table[(i,j)]
+    if i+1 == j:
+      return 0
+
+    res = 0
+    for ii in range(i+1, j):
+      tmp1 = self.helper(nums, i, ii)
+      tmp2 = self.helper(nums, ii, j)
+      res = max(res, tmp1+tmp2+nums[ii]*nums[i]*nums[j])
+    self.table[(i,j)] = res
+    return self.table[(i,j)]
+       
+
+if __name__ == "__main__":
+  a = Solution()
+  print a.maxCoins([3,1,5,8]) 
