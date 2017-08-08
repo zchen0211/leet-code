23aab6b73a0429f9b8f56d2bbefb0154ea5c922c
Zhuoyuan <chenzhuoyuan07@gmail.com>
2017-08-07 22:22:49 -0700

    42

---
+++ geometry/42_trap_rain.py
@@ -0,0 +1,37 @@
+"""
+42. Trapping Rain Water (Hard)
+
+Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+
+For example, 
+Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
+"""
+
+class Solution(object):
+  def trap(self, height):
+    """
+    :type height: List[int]
+    :rtype: int
+    """
+    n = len(height)
+    if n == 0: return 0
+    result = 0
+    stack = [(0, height[0])]
+    for i in range(1, n):
+      if height[i] < stack[-1][1]:
+        stack.append((i, height[i]))
+      else:
+        while len(stack)>0 and stack[-1][1]<height[i]:
+          ind, h = stack.pop()
+          if len(stack) > 0:
+            result += (i-stack[-1][0]-1) * (min(height[i], stack[-1][1]) - h)
+            print i, stack[-1][0], min(height[i], stack[-1][1]), h, result, 'stack', stack
+        stack.append((i, height[i]))
+      print 'i', i, height[i], 'result', result, 'stack', stack
+    return result
+
+
+if __name__ == "__main__":
+  a = Solution()
+  print a.trap([0,1,0,2,1,0,1,3,2,1,2,1])
+  # print a.trap([4,2,0,3,2,5])
