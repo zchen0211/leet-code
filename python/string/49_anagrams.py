aab5ed1fcd45e2eb2c26f33051dbbc61898b1cd1
zchen0211 <chenzhuoyuan07@gmail.com>
2017-09-25 18:06:44 -0700

    49

---
+++ python/string/49_anagrams.py
@@ -0,0 +1,37 @@
+'''
+49. Group Anagrams (Medium)
+
+Given an array of strings, group anagrams together.
+
+For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"], 
+Return:
+
+[
+  ["ate", "eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+'''
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        # notice! sort() modifies an array in place
+        # it returns None
+        mapping = {}
+        for str_ in strs:
+            re = list(str_)
+            re.sort()
+            re = ''.join(re)
+            if re in mapping:
+                mapping[re].append(str_)
+            else:
+                mapping[re] = [str_]
+        result = []
+        for k in mapping.keys():
+            tmp = list(mapping[k])
+            result.append(tmp)
+        return result        
