d883e9dc780b4219e96bf12ccdbe63f19a05e034
zchen0211 <chenzhuoyuan07@gmail.com>
2017-10-25 01:04:55 -0700

    5 new problems locked

---
+++ python/hash_table/277_find_celebrity.py
@@ -0,0 +1,75 @@
+"""
+277. Find the Celebrity (Medium)
+
+Suppose you are at a party with n people (labeled from 0 to n - 1) and among them, there may exist one celebrity. The definition of a celebrity is that all the other n - 1 people know him/her but he/she does not know any of them.
+
+Now you want to find out who the celebrity is or verify that there is not one. The only thing you are allowed to do is to ask questions like: "Hi, A. Do you know B?" to get information of whether A knows B. You need to find out the celebrity (or verify there is not one) by asking as few questions as possible (in the asymptotic sense).
+
+You are given a helper function bool knows(a, b) which tells you whether A knows B. Implement a function int findCelebrity(n), your function should minimize the number of calls to knows.
+
+Note: There will be exactly one celebrity if he/she is in the party. Return the celebrity's label if there is a celebrity in the party. If there is no celebrity, return -1.
+"""
+
+# The knows API is already defined for you.
+# @param a, person a
+# @param b, person b
+# @return a boolean, whether a knows b
+# def knows(a, b):
+
+know_set = set()
+know_set.add((0,1))
+know_set.add((0,2))
+know_set.add((1,2))
+
+def knows(a, b):
+  return (a, b) in know_set
+
+class Solution(object):
+    def findCelebrity(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <= 1: return n - 1
+        
+        # candidate list
+        cand = set(range(n))
+        
+        # step 1: 0 knows everyone?
+        for i in range(1, n):
+            tmp = knows(0, i)
+            if tmp:
+                if 0 in cand:
+                    cand.remove(0)
+            else:
+                cand.remove(i)
+                
+        # step 2: go through
+        for i in range(1, n):
+            remove_list = []
+            for j in cand:
+                if i == j:
+                    continue
+                tmp = knows(i, j)
+                if tmp:
+                    remove_list.append(i)
+                else:
+                    remove_list.append(j)
+            for j in remove_list:
+                if j in cand: cand.remove(j)
+            if len(cand) == 0: return -1
+        
+        # step 3: verify cand does not know anyone
+        cand = list(cand)
+        cand = cand[0]
+        for i in range(n):
+            if i == cand: continue
+            if knows(cand, i):
+                return -1
+        return cand
+
+print knows(0, 1)
+print knows(0, 2)
+print knows(1, 2)
+a = Solution()
+print a.findCelebrity(3)
